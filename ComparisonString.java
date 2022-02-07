7f8afdaa19bf8c831654962d1da19489baa8d610
Vivek Kumar <48044537+vivekuser@users.noreply.github.com>
2022-02-07 08:43:32 +0530

    Add files via upload

---
+++ ComparisonString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ComparisonString{
+
+	public static void main(String[] args) {
+		
+
+                    Scanner reader = new Scanner(System.in);
+
+                    System.out.println("Enter a string");
+                    String input = reader.nextLine();
+
+            if (input.equals("a string")) {
+                                             System.out.println("Great! You read the instructions correctly.");
+                                          } 
+           else     {
+                       System.out.println("Missed the mark!");
+                       }
+ }
+}
